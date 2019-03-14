0deb7d00424db8e462f1a133215beb749bc68aac
Anton Antonov <anton.antonov@qio.io>
2019-03-13 20:51:10 -0400

    Better tests.

--- OutlierIdentifiers/tests/testthat/test-outlieridentifiers.R
+++ OutlierIdentifiers/tests/testthat/test-outlieridentifiers.R
@@ -1,8 +1,11 @@
 context("Same results with different signatures")
 library(OutlierIdentifiers)
 
-randData <- runif( n = 100, min = -10, max = 100 )
-
+set.seed(2342)
+randData <- runif( n = 200, min = -10, max = 100 )
+randData2 <- runif( n = 19, min = 140, max = 160 )
+randData3 <- runif( n = 12, min = -50, max = -30 )
+randData <- sample( c( randData, randData2, randData3 ) )
 
 test_that("OutlierIdentifier equivalences", {
 
@@ -21,8 +24,8 @@ test_that("OutlierIdentifier equivalences", {
 test_that("TopOutlierIdentifier equivalences", {
 
   expect_equal(
-    TopOutlierIdentifier( data = randData, identifier = SPLUSQuartileIdentifierParameters ),
-    TopOutlierIdentifier( data = randData, lowerAndUpperThresholds = SPLUSQuartileIdentifierParameters(randData) )
+    TopOutlierIdentifier( data = randData, identifier = QuartileIdentifierParameters ),
+    TopOutlierIdentifier( data = randData, lowerAndUpperThresholds = QuartileIdentifierParameters(randData) )
   )
 
   expect_equal(
@@ -35,8 +38,8 @@ test_that("TopOutlierIdentifier equivalences", {
 test_that("BottomOutlierIdentifier equivalences", {
 
   expect_equal(
-    BottomOutlierIdentifier( data = randData, identifier = SPLUSQuartileIdentifierParameters ),
-    BottomOutlierIdentifier( data = randData, lowerAndUpperThresholds = SPLUSQuartileIdentifierParameters(randData) )
+    BottomOutlierIdentifier( data = randData, identifier = QuartileIdentifierParameters ),
+    BottomOutlierIdentifier( data = randData, lowerAndUpperThresholds = QuartileIdentifierParameters(randData) )
   )
 
   expect_equal(
@@ -49,13 +52,41 @@ test_that("BottomOutlierIdentifier equivalences", {
 test_that("OutlierPosition equivalences", {
 
   expect_equal(
-    OutlierPosition( data = randData, identifier = SPLUSQuartileIdentifierParameters ),
-    OutlierPosition( data = randData, lowerAndUpperThresholds = SPLUSQuartileIdentifierParameters(randData) )
+    OutlierPosition( data = randData, identifier = QuartileIdentifierParameters ),
+    OutlierPosition( data = randData, lowerAndUpperThresholds = QuartileIdentifierParameters(randData) )
   )
 
   expect_equal(
     OutlierPosition( data = randData, identifier = HampelIdentifierParameters ),
     OutlierPosition( data = randData, identifier = NULL, lowerAndUpperThresholds = HampelIdentifierParameters(randData) )
+  )
+
+})
+
+test_that("TopOutlierPosition equivalences", {
+
+  expect_equal(
+    TopOutlierPosition( data = randData, identifier = QuartileIdentifierParameters ),
+    TopOutlierPosition( data = randData, lowerAndUpperThresholds = QuartileIdentifierParameters(randData) )
+  )
+
+  expect_equal(
+    TopOutlierPosition( data = randData, identifier = HampelIdentifierParameters ),
+    TopOutlierPosition( data = randData, identifier = NULL, lowerAndUpperThresholds = HampelIdentifierParameters(randData) )
+  )
+
+})
+
+test_that("BottomOutlierPosition equivalences", {
+
+  expect_equal(
+    BottomOutlierPosition( data = randData, identifier = QuartileIdentifierParameters ),
+    BottomOutlierPosition( data = randData, lowerAndUpperThresholds = QuartileIdentifierParameters(randData) )
+  )
+
+  expect_equal(
+    BottomOutlierPosition( data = randData, identifier = HampelIdentifierParameters ),
+    BottomOutlierPosition( data = randData, identifier = NULL, lowerAndUpperThresholds = HampelIdentifierParameters(randData) )
   )
 
 })
