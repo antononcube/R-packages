41ca227111cb7d25f5c84110b1df18bfd17cfd64
antonocube <antononcube@gmail.com>
2019-03-28 10:20:35 -0700

    Added text Hamlet data.

---
+++ LSAMon-R/R/data.R
@@ -0,0 +1,9 @@
+#' THE TRAGEDY OF HAMLET, PRINCE OF DENMARK by William Shakespeare
+#'
+#' The text of Shakespeare's play Hamlet is split into parts that
+#' correspond to the dialogue in the play.
+#'
+#' @format A character vector
+#'
+#' @source \url{http://erdani.com/tdpl/hamlet.txt}
+"textHamlet"
