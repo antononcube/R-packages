5bb5c0357a70eb546ed0ccfecaf6169797d24f67
antonocube <antononcube@gmail.com>
2019-03-30 06:27:20 -0700

    Better tests.

--- LSAMon-R/tests/testthat/test_basic_pipeline.R
+++ LSAMon-R/tests/testthat/test_basic_pipeline.R
@@ -1,9 +1,8 @@
 context("Basic pipeline")
 library(LSAMon)
 
-
 lsaObj <-
-  LSAMonUnit( textHamlet ) %>%
+  LSAMonUnit( tolower(textHamlet) ) %>%
   LSAMonMakeDocumentTermMatrix( stopWords = NULL, splitPattern = "\\W") %>%
   LSAMonApplyTermWeightFunctions()
 
@@ -13,14 +12,31 @@ test_that("Sanity check", {
   expect_true( length( intersect( c("Value", "Documents", "DocumentTermMatrix", "W", "H"), names(lsaObj) ) ) == 5 )
 })
 
+words <- unique(c(unlist(strsplit(tolower(textHamlet), "\\W"))))
+words <- words[ nchar(words) > 0 ]
+
+test_that("Document-term matrix check", {
+  expect_true( length(words) == ncol(lsaObj %>% LSAMonTakeDocumentTermMatrix) )
+  expect_true( mean(words %in% colnames(lsaObj %>% LSAMonTakeDocumentTermMatrix) ) == 1 )
+})
+
+lsaObj <-
+  lsaObj %>%
+  LSAMonTopicExtraction( minNumberOfDocumentsPerTerm = 1, numberOfTopics = 60, maxSteps = 6, profiling = FALSE )
+
 dfTopic <-
   lsaObj %>%
-  LSAMonTopicExtraction( numberOfTopics = 60, maxSteps = 20, profiling = FALSE ) %>%
   LSAMonBasisVectorInterpretation(vectorIndices = 30, n = 30) %>%
   LSAMonTakeValue
 
 ## Tests
 test_that("Topic extraction", {
+  expect_true( nrow(lsaObj %>% LSAMonTakeW) == nrow(lsaObj %>% LSAMonTakeDocumentTermMatrix) )
+  expect_true( ncol(lsaObj %>% LSAMonTakeW) == 60 )
+
+  expect_true( nrow(lsaObj %>% LSAMonTakeH) == 60 )
+  expect_true( ncol(lsaObj %>% LSAMonTakeH) == ncol(lsaObj %>% LSAMonTakeDocumentTermMatrix) )
+
   expect_is( dfTopic, "data.frame" )
   expect_true( length( intersect( c("Rank", "SignificanceFactor", "Term", "Coefficient"), colnames(dfTopic) ) ) == 4 )
 })
