061a152c51abaff9d08ee3e9f7d85b3a1cab0524
antonocube <antononcube@gmail.com>
2020-08-15 05:24:06 -0400

    Better layout.

--- DataQueryWorkflowsTests/tests/testthat/test-Basic-commands-tidyverse.R
+++ DataQueryWorkflowsTests/tests/testthat/test-Basic-commands-tidyverse.R
@@ -1,89 +1,135 @@
-context("Basic operations tidyverse")
+context("Basic commands tidyverse")
 library(tidyverse)
 library(DataQueryWorkflowsTests)
+library(ExternalParsersHookUp)
 
 
 test_that("Select columns", {
 
   ##
-  lsCommands <- "use dfStarwars; select name, mass and height"
+  lsCommands <- "use dfStarwars;
+  select name, mass and height"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::select(name, mass, height) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::select(name, mass, height)
+  )
+
 })
 
 
 test_that("Mutate columns", {
   ##
-  lsCommands <- "use dfStarwars; mutate bmi = `mass/height^2`"
+  lsCommands <- "use dfStarwars;
+  mutate bmi = `mass/height^2`"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::mutate( bmi = mass / height ^ 2 ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::mutate( bmi = mass / height ^ 2 )
+  )
 
   ##
-  lsCommands <- "use dfStarwars; transform bmi = `mass/height^2`"
+  lsCommands <- "use dfStarwars;
+  transform bmi = `mass/height^2`"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::mutate( bmi = mass / height ^ 2 ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::mutate( bmi = mass / height ^ 2 )
+  )
+
 })
 
 
 test_that("Rename columns", {
   ##
-  lsCommands <- "use dfStarwars; rename mass, height, and gender as mass2, height2, gender2"
+  lsCommands <- "use dfStarwars;
+  rename mass, height, and gender as mass2, height2, gender2"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::rename( c("mass2" = "mass", "height2" = "height", "gender2" = "gender" ) ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::rename( c("mass2" = "mass", "height2" = "height", "gender2" = "gender" ) )
+  )
+
 })
 
 
 test_that("Drop columns", {
   ##
-  lsCommands <- "use dfStarwars; drop columns species, name"
+  lsCommands <- "use dfStarwars;
+  drop columns species, name"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::mutate( species = NULL, name = NULL ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::mutate( species = NULL, name = NULL )
+  )
 
 })
 
 
 test_that("Arrange columns", {
+
   ##
-  lsCommands <- "use dfStarwars; arrange by species, gender, name"
+  lsCommands <- "use dfStarwars;
+  arrange by species, gender, name"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::arrange( species, gender, name ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::arrange( species, gender, name )
+  )
 
   ##
-  lsCommands <- "use dfStarwars; sort descending by species, gender, name "
+  lsCommands <- "use dfStarwars;
+  sort descending by species, gender, name "
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::arrange( desc(species, gender, name) ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::arrange( desc(species, gender, name) )
+  )
 
 })
 
 
 test_that("Filter rows", {
+
   ##
-  lsCommands <- "use dfStarwars; filter by mass is greater or equal than 200 and height is greater than 100 or height is 202"
+  lsCommands <- "use dfStarwars;
+  filter by mass is greater or equal than 200 and height is greater than 100 or height is 202"
+
   res <- ToDataQueryWorkflowCode( lsCommands )
 
   expect_is( res, "expression" )
 
-  expect_equivalent( eval(res), dfStarwars %>% dplyr::filter( mass >= 200 & height > 100 | height == 202 ) )
+  expect_equivalent(
+    eval(res),
+    dfStarwars %>% dplyr::filter( mass >= 200 & height > 100 | height == 202 )
+  )
 
 })
