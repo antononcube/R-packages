dcf573ae52a196f40e0b2035864f69512039f4d0
antonocube <antononcube@gmail.com>
2021-04-12 13:45:23 -0400

    Tests for the transformer(s) functions.

---
+++ RandomDataFrameGenerator/tests/testthat/test-transformers.R
@@ -0,0 +1,35 @@
+library(RandomDataFrameGenerator)
+
+set.seed(3353)
+numberOfUsers <- 3
+numberOfIngredients <- 3
+numberOfDays <- 90
+numberOfItems <- 20
+
+test_that("Variables split", {
+
+  expect_s3_class( object =
+                     dfRand <-
+                     RandomDataFrame(
+                       nrow = numberOfItems,
+                       columnNames = c("ID", "UserID", "PeriodMeal", "Ingredient", "Cuisine", "Timestamp"),
+                       generators =
+                         list(
+                           ID = function(n) as.character(round(runif(n = n, min = 10^6, max = 16^8 ))),
+                           UserID = RandomString(numberOfUsers, charClasses = "[a-z]"),
+                           PeriodMeal = c("lunch", "dinner", "breakfast"),
+                           Ingredient = function(n) purrr::map_chr(1:n, function(x) paste( sample( x = c("protein", "carb", "low calorie", "fat", "saturated fat"), prob = c(1, 2, 0.5, 1, 0.4), size = numberOfIngredients, replace = F), collapse = "; " )),
+                           Cuisine = c("Chinese", "Finnish", "Turkish", "Greek", "Scandinavian"),
+                           Timestamp = function(n) as.character(sort(RandomDate(size = n, min = Sys.time(), max = Sys.time() + numberOfDays*24*3600)))
+                         ),
+                       form = "Long"
+                     ), class = "data.frame" )
+
+
+  expect_equal( nrow(dfRand), numberOfItems * 6 )
+
+  expect_s3_class( object = dfRand2 <- SplitVariableValues( data = dfRand, splitPattern = ";", variableToSplit = "Ingredient", valueColumn = "Value.character" ), "data.frame" )
+
+  expect_equal( nrow(dfRand2), nrow(dfRand) + numberOfItems * (numberOfIngredients - 1) )
+
+})
