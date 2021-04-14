84c53dbe733942430a8b32e5cb09f2bb57739a6c
antonocube <antononcube@gmail.com>
2021-04-14 09:27:28 -0400

    First version.

---
+++ RecommenderDashboards/R/CRAN-and-GitHub-installation.R
@@ -0,0 +1,17 @@
+
+install.packages(c("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "irlba", "NutrienTrackeR", "devtools"))
+
+devtools::install_github( repo = "rstudio/d3heatmap")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataFrameGenerator")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "NonNegativeMatrixFactorization")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R")
+devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataFrameGenerator")
+
+
+
+
---
+++ RecommenderDashboards/README.md
@@ -0,0 +1,19 @@
+# Shiny interfaces
+
+## Introduction
+
+This repository has code and data for different interactive interfaces made with 
+[RStudio's Shiny](https://shiny.rstudio.com).
+
+## Installation
+
+First install the R packages in the script: 
+[CRAN-and-GitHub-installation.R](https://bitbucket.org/connectedentsearch/shinyinterfaces/src/master/R/CRAN-and-GitHub-installation.R).
+
+(Open that file in RStudio and run it. Alternatively, using a command line tool run it with `Rscript`.)
+
+## The example interface
+
+The example interface is "ready to go" -- just open the file 
+[GenericRecommenderInterface.Rmd](https://bitbucket.org/connectedentsearch/shinyinterfaces/src/master/flexdashboards/GenericRecommenderInterface.Rmd)
+in RStudio and run it. (By pressing the button "Run Document".)
