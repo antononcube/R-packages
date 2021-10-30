3735953ea6d71f636f288aa141360069c6cfd96b
Anton Antonov <anton.antonov@qio.io>
2021-10-30 16:26:36 -0400

    build:Added feater. Added options for devtools::install_github.

--- RecommenderDashboards/R/CRAN-and-GitHub-installation.R
+++ RecommenderDashboards/R/CRAN-and-GitHub-installation.R
@@ -5,19 +5,19 @@
 #' @details The GitHub packages from "antononcube/R-packages" are
 #' provide the recommender and LSA objects that showcased in the flexdashboard(s).
 #' @export
-InstallRequiredPackages <- function( gitHubOnlyQ = FALSE ) {
+InstallRequiredPackages <- function( gitHubOnlyQ = FALSE, ... ) {
 
   if( !gitHubOnlyQ ) {
-    install.packages(c("tidyverse", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "irlba", "NutrienTrackeR", "devtools"))
+    install.packages(c("tidyverse", "purrr", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "irlba", "NutrienTrackeR", "devtools", "feather"))
   }
 
-  devtools::install_github( repo = "rstudio/d3heatmap")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataFrameGenerator")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "NonNegativeMatrixFactorization")
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R")
+  devtools::install_github( repo = "rstudio/d3heatmap", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataFrameGenerator", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "NonNegativeMatrixFactorization", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R", ...)
 }
