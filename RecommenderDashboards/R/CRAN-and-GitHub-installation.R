f8517d49de886b867b51428004cf40e5ff911237
antononcube <antononcube@posteo.net>
2022-09-09 13:36:19 -0400

    fix:Correct repository name.

--- RecommenderDashboards/R/CRAN-and-GitHub-installation.R
+++ RecommenderDashboards/R/CRAN-and-GitHub-installation.R
@@ -11,12 +11,13 @@ InstallRequiredPackages <- function( gitHubOnlyQ = FALSE, ... ) {
     install.packages(c("tidyverse", "purrr", "rmarkdown", "shiny", "flexdashboard", "DT", "stopwords", "SnowballC", "irlba", "NutrienTrackeR", "devtools", "feather"))
   }
 
-  devtools::install_github( repo = "rstudio/d3heatmap", ...)
-  devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataFrameGenerator", ...)
+  #devtools::install_github( repo = "rstudio/d3heatmap", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "RandomDataGenerators", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "ParetoPrincipleAdherence", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "OutlierIdentifiers", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommender", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "SMRMon-R", ...)
+  devtools::install_github( repo = "antononcube/R-packages", subdir = "GNNMon-R", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "SparseMatrixRecommenderInterfacesNoDT", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "NonNegativeMatrixFactorization", ...)
   devtools::install_github( repo = "antononcube/R-packages", subdir = "LSAMon-R", ...)
